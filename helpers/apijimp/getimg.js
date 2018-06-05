e61182ea791f70bdba2804f20de54b72d85661f6
Desvelao <iamdesvelao@gmail.com>
2018-06-05 18:44:30 +0200

    fixed apijimp aegis img

--- helpers/apijimp/getimg.js
+++ helpers/apijimp/getimg.js
@@ -2,7 +2,8 @@ const iids = require('./items_id.json')
 const hids = require('./heroes_id.json')
 
 module.exports.getItem = function(id){
-  return iids[id] || null
+  const item = iids[id] || null
+  return !item.includes('recipe') ? iids[id] : null
 }
 
 module.exports.getHero = function(id){
