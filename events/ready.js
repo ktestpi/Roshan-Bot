450363779f456b3a8009638ff917ae245ac89c15
Desvelao <iamdesvelao@gmail.com>
2018-10-27 01:14:01 +0200

    fix diretide launch

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ const { sortTourneys, updateAccountSchema, resetServerConfig } = require('../hel
 const package = require('../package.json')
 const opendota = require('../helpers/opendota')
 const path = require('path')
-const Diretide = require('../seasonal/diretide')
+const Diretide = require('../seasonal/diretide/index.js')
 
 module.exports = new Event('ready','ready',{}, function(){
   if(this._started){return}else{this._started = true}
