848990f26ea63370792bdb2b2450e593554d4212
Desvelao <iamdesvelao@gmail.com>
2018-11-28 00:13:25 +0100

    Artifact countdown interval 2h

--- plugins/funs.js
+++ plugins/funs.js
@@ -7,35 +7,28 @@ module.exports = class Funcs extends Plugin {
     constructor(client, options) {
         super(client)
         this.endDate = (new Date('Wed, 28 Nov 2018 22:00:00')).getTime()
-        console.log('Dates',this.endDate,new Date().getTime())
         // this.endDate = new Date('2018-11-28T23:00:00.000Z').getTime()
         this.remaining = () => {
             return Math.round((this.endDate - (new Date()).getTime())/1000)
         }
         this.channel = '474935110567985152'
-        this.repeatEach = 3600
-        this.starterTimer = this.remaining() % this.repeatEach
-        console.log(this.starterTimer)
+        this.repeatEach = 7200
     }
     ready(){
-        this.setTimeout(this.starterTimer)
+        const starterTimer = this.remaining() % this.repeatEach
+        this.setTimeout(starterTimer)
     }
-    setTimeout(time = 3600){
-        this.timer = new Timeout(() => this.func(this.repeatEach), time * 1000)
-        // this.timer = setTimeout(() => this.func(),time*1000)
+    setTimeout(time = 7200){
+        this.timer = new Timeout(() => this.func(), time * 1000)
     }
-    func(time = 3600){
+    func(){
         const coming = this.remaining()
-        if(coming > 0){
+        this.timer.clear()
+        if (coming > this.repeatEach){
             this.client.createMessage(this.channel, this.client.locale.replacer(`<artifact> Artifact sale en <cooldown> ${secondsToHms(coming)}`))
-            this.timer.clear()
-            if(coming > this.repeatEach){
-                this.setTimeout(this.repeatEach)
-            }else{
-                this.timer = new Timeout(() => this.client.createMessage(this.channel, this.client.locale.replacer(`<artifact> Artifact está en cualquier momento!`)), coming * 1000)
-            }
+            this.setTimeout(this.repeatEach)
         }else{
-            this.timer.clear()
+            this.timer = new Timeout(() => this.client.createMessage(this.channel, this.client.locale.replacer(`<artifact> Artifact está en cualquier momento!`)), coming * 1000)
         }
     }
 }
