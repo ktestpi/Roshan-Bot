783761aa57debc0fe4240c1bdab3fab8a92f1e36
Desvelao <iamdesvelao@gmail.com>
2018-12-22 17:11:18 +0100

    Change patreon cmd to donate + added img_assets

--- commands/general/patreon.js
+++
@@ -1,7 +0,0 @@
-const { Command } = require('aghanim')
-
-module.exports = new Command(['patreon','donate'],{
-  category : 'General', help : '❤ Dona para apoyar el bot', args : ''},
-  function(msg, args, command){
-    return msg.reply(this.locale.replacer(this.locale.getUserString('linkPatreon',msg),{link : this.config.links.patreon}))
-  })
