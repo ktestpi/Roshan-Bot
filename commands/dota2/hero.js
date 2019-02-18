ef2f63a4ede2e6831cec122fd00a76fbca11b2ba
Desvelao <iamdesvelao@gmail.com>
2019-02-18 20:26:01 +0100

    added playercard bg and fix hero command

--- commands/dota2/hero.js
+++ commands/dota2/hero.js
@@ -6,8 +6,8 @@ const embed = new EmbedBuilder({
     author: { name: '<_hero_name>', icon_url: '<_hero_icon>', url: '<_hero_wikiurl>'},
     thumbnail: {url: '<_hero_image>'},
     fields: [
-        { name: 'Stats', value: 'Base health <_hero_basehealth>\nBase mana <_hero_basemana>\nBase armor <_hero_basearmor>\nAttack type <_hero_attacktype>\nRange <_hero_range>\nMS <_hero_ms>\n', inline: true},
-        { name: 'Attributes', value: '<_str_ps>Str<_str_ps>:<_str> + <_str_gain>\n<_agi_ps>Agi<_agi_ps>:<_agi> + <_agi_gain>\n<_int_ps>Int<_int_ps>:<_int> + <_int_gain>', inline: true},
+        { name: 'Stats', value: 'Base health: <_hero_basehealth>\nBase mana: <_hero_basemana>\nBase armor: <_hero_basearmor>\nAttack type: <_hero_attacktype>\nRange: <_hero_range>\nMS: <_hero_ms>\n', inline: true},
+        { name: 'Attributes', value: '<_str_ps>Str<_str_ps>: <_str> + <_str_gain>\n<_agi_ps>Agi<_agi_ps>: <_agi> + <_agi_gain>\n<_int_ps>Int<_int_ps>: <_int> + <_int_gain>', inline: true},
         { name: 'Roles', value: '<_roles>', inline: true},
         { name: 'Abilities', value: '<_abilities>', inline: true}
     ]
@@ -36,6 +36,10 @@ module.exports = new Command('hero', {
             _int: hero.base_int,
             _int_gain: hero.int_gain,
             _roles: hero.roles.join(', '),
-            _abilities: hero.abilities.join(', ')
+            _abilities: hero.abilities.join(', '),
+            _hero_image: `${enumHeroes.dotaCdnURL}${hero.img}`,
+            _hero_name: hero.localized_name,
+            _hero_basemana: hero.base_mana + ((hero.primary_attr === "int" ? 15 : 12) * hero.base_int),
+            _hero_basearmor: hero.base_armor + Math.round((hero.primary_attr === "agi" ? 0.2 : 0.16) * hero.base_agi)
         }))
     })
--- enums/card_bg.js
+++ enums/card_bg.js
@@ -2,7 +2,12 @@ const { SimpleEnums } = require('../classes/enums')
 
 const card_bg = {
   '0' : 'roshan',
-  '1' : 'mheroes'
+  '1' : 'mheroes',
+  '2' : 'dotalogo',
+  '3' : 'io',
+  '4' : 'pa',
+  '5' : 'phoenix',
+  '6' : 'qop'
 }
 
 module.exports = new SimpleEnums(card_bg)
