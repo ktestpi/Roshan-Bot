db127400d95cb05838b8e348c16dde80c81a6a95
Desvelao <iamdesvelao@gmail.com>
2020-03-07 12:09:08 +0100

    Fixes some string substitutions

--- commands/opendota/competitive.js
+++ commands/opendota/competitive.js
@@ -10,7 +10,7 @@ module.exports = {
     return client.components.Opendota.competitive()
       .then(results => {
         const spacesBoard = ['19f', '2f', '19f', '17f', '11f']
-        let table = Classes.Table.renderRow([msg.author.locale('dota2.radiant'), msg.author.locale('dota2.w'), msg.author.locale('dire'), msg.author.locale('dota2.league'), msg.author.locale('dota2.matchID')], spacesBoard, '\u2002') + '\n';
+        let table = Classes.Table.renderRow([msg.author.locale('dota2.radiant'), msg.author.locale('dota2.w'), msg.author.locale('dota2.dire'), msg.author.locale('dota2.league'), msg.author.locale('dota2.matchid')], spacesBoard, '\u2002') + '\n';
         results[0].slice(0,8).forEach(match => {
           const victory = match.radiant_win ? '>>' : '<<'
           table += Classes.Table.renderRow([client.components.Bot.parseText(match.radiant_name, 'nf'), victory, client.components.Bot.parseText(match.dire_name, 'nf'), client.components.Bot.parseText(match.league_name, 'nf')], spacesBoard, '\u2002') + ' ' + Markdown.link('https://www.dotabuff.com/matches/' + match.match_id, match.match_id) + '\n';    
